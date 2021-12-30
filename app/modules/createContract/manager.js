278359ceeb3a7df13d1e9bc950dd75362601a744
shivanib <shivanib@leewayhertz.com>
2021-12-30 20:19:37 +0530

    Api for creating xrc-20 token

---
+++ app/modules/createContract/manager.js
@@ -0,0 +1,10 @@
+const db = require('../../../database/models/index');
+// const user = db.User;
+export default class Manager {
+    createXrc20Token = async (requestData) => {
+        // API business logic
+
+        return {};
+
+    }
+}
