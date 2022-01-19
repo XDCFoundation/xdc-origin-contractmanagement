7a956e999635474b9dc44848319ae98f7048b406
shivanib <shivanib@leewayhertz.com>
2022-01-19 16:00:50 +0530

    Key for 'isVerified' in the token model

---
+++ database/migrations/20220119102441-add_verified_key.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.all([ queryInterface.addColumn(
+        'XRC20Tokens',
+        'isVerified',
+        Sequelize.BOOLEAN,
+    )
+    ]);
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
