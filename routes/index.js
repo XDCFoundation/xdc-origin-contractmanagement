ff0558fab471a838f1e6dc3119874587ef03dbaa
shivanib <shivanib@leewayhertz.com>
2022-02-04 23:57:10 +0530

    isPaused key added to the xrc20 token model and api for pausing and resuming a token

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,6 @@ module.exports = (app) => {
     app.post("/verify-xrc20-token", ValidationManger.validateVerifyXrc20Token, new CreateContractModule().verifyXrc20Token);
     app.post("/get-deployed-xrc20token", ValidationManger.validateGetDraftXrc20Token, new CreateContractModule().getDeployedXRC20Token);
     app.post("/mint-burn-xrc20Token", ValidationManger.validateMintBurnXrc20Token, new CreateContractModule().mintBurnXrc20Token);
+    app.post("/pause-resume-xrc20-token", ValidationManger.validatePauseResumeXrc20Token, new CreateContractModule().pauseResumeXrc20Token);
+
 };
